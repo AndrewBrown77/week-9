7d3ccd946942a7f9c9da25086b7f438cc767af0b
andrew-brown <andrewmbrown97@gmail.com>
2024-07-30 21:38:05 -0400

    commit

--- index.js
+++ index.js
@@ -19,67 +19,34 @@ function promptUser() {
       name: "description",
       message: "What is the description of your project?"
     },
-    // {
-    //   type: "checkbox",
-    //   message: "Check which sections are needed for your table of contents",
-    //   name: "contents",
-    //   choices: [
-    //     "Title",
-    //     "Description",
-    //     "Installation",
-    //     "Use",
-    //     "License",
-    //     "Contributions",
-    //     "Tests",
-    //     "Questions",
-    //   ]
-    // },
-    // {
-    //   type: "input",
-    //   name: "install",
-    //   message: "What is the installation instructions?"
-    // },
-    // {
-    //   type: "input",
-    //   name: "use",
-    //   message: "How is this project used?"
-    // },
-    // {
-    //   type: "list",
-    //   message: "Which type of license would you like to use?",
-    //   name: "license",
-    //   choices: [
-    //     "Apache License 2.0", 
-    //     "MIT License", 
-    //     "Boost Software License 1.0", 
-    //     "Eclipse Public License 2.0"
-    //   ]
-    // },
-    // {
-    //   type: "input",
-    //   name: "name",
-    //   message: "What is your first and last name for copyright license?"
-    // },
-    // {
-    //   type: "input",
-    //   name: "contributions",
-    //   message: "Please list any contributions made to the project."
-    // },
-    // {
-    //   type: "input",
-    //   name: "test",
-    //   message: "What is the test instructions for the project?"
-    // },
-    // {
-    //   type: "input",
-    //   name: "questions",
-    //   message: "Please enter your GitHub username."
-    // },
-    // {
-    //   type: "input",
-    //   name: "email",
-    //   message: "Please enter your contact Email?"
-    // },
+    {
+      type: "input",
+      name: "install",
+      message: "What is the installation instructions?"
+    },
+// {
+//   type: "input",
+//   message: "How is this project used?"
+//   },
+    {
+      type: "list",
+      message: "Which type of license would you like to use?",
+      name: "license",
+      choices: [
+        "Apache License 2.0", 
+        "MIT License", 
+        "Boost Software License 1.0", 
+        "Eclipse Public License 2.0"
+      ]
+    },
+
+    
+
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your contact Email?"
+    },
   ]);
 }
 
@@ -91,10 +58,6 @@ function generateREAD(answers) {
   ## Description 
 
   ${answers.description}
-  
-  ## Table of Contents 
-  
-  ${answers.contents}
   
   ## Installation 
   
